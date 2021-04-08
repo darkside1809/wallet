8ba4aef8ab68f3decb35991cd06a76aba65a696d
Nekruz <njpnek@gmail.com>
2021-04-08 16:59:47 +0500

    add tests and fix functions

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -2,20 +2,97 @@ package wallet
 
 import (
 	"github.com/darkside1809/wallet/pkg/types"
+	"github.com/google/uuid"
 	"errors"
 )
 var ErrPhoneRegistered = errors.New("phone already registered")
 var ErrAmountMustBePositive = errors.New("amount must be greater than 0")
+var ErrNotEnoughBalance = errors.New("not enought balance in account")
 var ErrAccountNotFound = errors.New("account not found")
+var ErrPaymentNotFound = errors.New("payment not found")
 var exErr error = nil
 
 type Service struct {
-	nextAccountId 	int64
+	nextAccountID 	int64
 	accounts 		[]*types.Account
 	payments 		[]*types.Payment
 }
 
-func (s *Service)FindAccountById(account int64) (*types.Account, error){
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+
+	s.nextAccountID++
+	account := &types.Account{
+		ID:      s.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+
+	return account, nil
+}
+
+func (s *Service) Deposite(accountID int64, amount types.Money) error {
+	if amount <= 0 {
+		return ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return ErrAccountNotFound
+	}
+
+	account.Balance += amount
+	return nil
+}
+
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	if account.Balance < amount {
+		return nil, ErrNotEnoughBalance
+	}
+
+	account.Balance -= amount
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID:        paymentID,
+		AccountID: accountID,
+		Amount:    amount,
+		Category:  category,
+		Status:    types.PaymentStatusInProgress,
+	}
+	s.payments = append(s.payments, payment)
+
+	return payment, nil
+}
+
+func (s *Service)FindAccountByID(account int64) (*types.Account, error){
 	var accID *types.Account
 
 	for _, acc := range s.accounts{
@@ -31,4 +108,46 @@ func (s *Service)FindAccountById(account int64) (*types.Account, error){
 	}
 	
 	return accID, nil
+}
+
+func (s *Service)FindPaymentByID(paymentID string) (*types.Payment, error) {
+	var payment *types.Payment
+
+	for _, pay := range s.payments {
+		if pay.ID == payment.ID {
+			payment = pay
+			break
+		}
+		if payment == nil {
+			return nil, ErrPaymentNotFound
+		}
+	}
+
+	return payment, nil
+}
+
+func (s *Service)Reject(paymentID string) error {
+	var payCheck *types.Payment
+
+	for _, paymentData := range s.payments {
+		if paymentID == payCheck.ID {
+			payCheck = paymentData
+			break
+		}
+	}
+	if payCheck == nil {
+		return  ErrPaymentNotFound
+	}
+
+	account, err := s.FindAccountByID(payCheck.AccountID)
+
+	if err == ErrAccountNotFound {
+		return ErrAccountNotFound
+	}
+
+	account.Balance += payCheck.Amount
+	payCheck.Amount = 0
+	payCheck.Status = types.PaymentStatusFail
+
+	return nil
 }
--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -6,6 +6,30 @@ import (
 	"github.com/darkside1809/wallet/pkg/types"
 )
 
+func TestService_FindAccountByID_success(t *testing.T) {
+	svc := &Service{}
+
+	account, _ := svc.RegisterAccount("+992000000001")
+
+	acc, err := svc.FindAccountByID(account.ID)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(account, acc) {
+		t.Error("Accounts not found")
+	}
+}
+func TestService_FindAccountByID_notFound(t *testing.T) {
+	svc := &Service{}
+
+	_, err := svc.FindAccountByID(123)
+
+	if err != ErrAccountNotFound {
+		t.Error(err)
+	}
+}
 func Test_FindAccountByID_success(t *testing.T) {
 	svc := &Service{
 		accounts: []*types.Account{
@@ -32,7 +56,7 @@ func Test_FindAccountByID_success(t *testing.T) {
 		Phone:   "992918632026",
 		Balance: 2_000,
 	}
-	account, err := svc.FindAccountById(2)
+	account, err := svc.FindAccountByID(2)
 
 	if !reflect.DeepEqual(expected, account) {
 		t.Errorf("invalid result, expected: %v, actual %v", expected, account)
@@ -65,7 +89,7 @@ func Test_FindAccountByID_accountNotFound(t *testing.T) {
 	}
 
 	var expected *types.Account = nil
-	account, err := svc.FindAccountById(5)
+	account, err := svc.FindAccountByID(5)
 
 	if !reflect.DeepEqual(expected, account) {
 		t.Errorf("invalid result, expected: %v, actual %v", expected, account)
@@ -73,5 +97,39 @@ func Test_FindAccountByID_accountNotFound(t *testing.T) {
 
 	if ErrAccountNotFound != err {
 		t.Errorf("invalid result, expected: %v, actual %v", ErrAccountNotFound, err)
+	}
+}
+
+func Test_Reject_success(t *testing.T) {
+	svc := &Service{}
+
+	account, err := svc.RegisterAccount("992918632026")
+
+	if err == ErrPhoneRegistered {
+		t.Error(ErrPhoneRegistered)
+	}
+
+	_, errPay := svc.Pay(account.ID, 1000, "auto")
+
+	if errPay != ErrNotEnoughBalance {
+		t.Error(ErrNotEnoughBalance)
+	}
+
+}
+
+
+func Test_Reject_paymentNotFound(t *testing.T) {
+	svc := &Service{}
+
+	account, err := svc.RegisterAccount("992918632026")
+
+	if err == ErrPhoneRegistered {
+		t.Error(ErrPhoneRegistered)
+	}
+
+	_, errPay := svc.Pay(account.ID, 2000, "auto")
+
+	if errPay != ErrNotEnoughBalance {
+		t.Error(ErrNotEnoughBalance)
 	}
 }
