4ca1b5daa837804677f47cffc163ff72c5811255
Nekruz <njpnek@gmail.com>
2021-04-30 19:02:34 +0500

    add SumWithProgress method and add Progress struct in types

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-
-	//"github.com/darkside1809/wallet/pkg/wallet"
+	"log"
+	"sync"
+	// "github.com/darkside1809/wallet/pkg/types"
+	// "github.com/darkside1809/wallet/pkg/wallet"
 )
 
 func main() {
@@ -12,6 +14,55 @@ func main() {
 	// if err != nil {
 	// 	log.Print(err)
 	// }
+	// err := svc.Export("data")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	// err = svc.Import("data")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	// err = svc.ExportToFile("data")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	// err = svc.ImportFromFile("data")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
 
+	// account, err := svc.ExportAccountHistory(125)
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	// log.Print(account)
 
+	// payments := []types.Payment{}
+	// err = svc.HistoryToFiles(payments, "data", 125)
+	
+	// err = svc.ImportFromFile("data")
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+
+	log.Print("main started")
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	sum := 0
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			sum++
+		}
+	}()	
+	go func() {
+		wg.Done()
+		for i := 0; i < 1000; i++ {
+			sum++
+		}
+	}()	
+	
+	wg.Wait()
+	log.Print("main finished")
+	log.Print(sum)
 }
